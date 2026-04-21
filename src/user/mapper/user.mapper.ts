861d6d01317e7566e87a0570880a44c4d9a6fcc4
matheusgondra <matheusgondra951@gmail.com>
2026-04-21 12:34:56 -0300

    feat: ensure UserRepository returns null if user does not exists

---
+++ src/user/mapper/user.mapper.ts
@@ -0,0 +1,16 @@
+import { User as UserEntity } from "@/generated/prisma/client";
+import { User } from "../entities/user.entity";
+
+export class UserMapper {
+	static toDomain(entity: UserEntity): User {
+		return new User({
+			id: entity.id,
+			firstName: entity.firstName,
+			lastName: entity.lastName,
+			email: entity.email,
+			password: entity.password,
+			createdAt: entity.createdAt,
+			updatedAt: entity.updatedAt
+		});
+	}
+}
---
+++ src/user/repositories/user.repository.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@/prisma/services/prisma.service";
+import { User } from "../entities/user.entity";
+import { LoadUserByEmailRepository } from "../protocols/load-user-by-email-repository.protocol";
+
+@Injectable()
+export class UserRepository implements LoadUserByEmailRepository {
+	constructor(private readonly prismaService: PrismaService) {}
+
+	async loadByEmail(email: string): Promise<User | null> {
+		const user = await this.prismaService.user.findUnique({
+			where: { email }
+		});
+		if (!user) {
+			return null;
+		}
+
+		return null;
+	}
+}
