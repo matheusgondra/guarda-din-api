35fd7ade41b99e7456a2c31d4850bc8bd58a5fae
matheusgondra <matheusgondra951@gmail.com>
2026-04-21 12:00:11 -0300

    feat: add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { validateEnv } from "./env.validation";
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationOptions: {
+				allowUnknown: false,
+				abortEarly: true
+			},
+			validate: validateEnv
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
---
+++ src/env.validation.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+const envSchema = z.object({
+	NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+	PORT: z.coerce.number().positive().int().default(3000),
+	DATABASE_URL: z.url()
+});
+
+export type Env = z.infer<typeof envSchema>;
+
+export const validateEnv = (config: Record<string, unknown>) => envSchema.parse(config);
