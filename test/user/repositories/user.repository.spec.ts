93d89ac00988b3c3774bf16275dc71263c5de92e
matheusgondra <matheusgondra951@gmail.com>
2026-04-21 12:36:43 -0300

    test: ensure UserRepository throws if PrismaService throw

--- test/user/repositories/user.repository.spec.ts
+++ test/user/repositories/user.repository.spec.ts
@@ -6,6 +6,7 @@ describe("UserRepository", () => {
 	const email = "any@email.com";
 
 	let sut: UserRepository;
+	let prismaService: PrismaService;
 
 	beforeEach(async () => {
 		const module = await Test.createTestingModule({
@@ -23,13 +24,22 @@ describe("UserRepository", () => {
 		}).compile();
 
 		sut = module.get<UserRepository>(UserRepository);
+		prismaService = module.get<PrismaService>(PrismaService);
 	});
 
 	describe("loadByEmail", () => {
-		it("should return null if user does not exist", async () => {
+		it("Should return null if user does not exist", async () => {
 			const result = await sut.loadByEmail(email);
 
 			expect(result).toBeNull();
+		});
+
+		it("Should throw if PrismaService throws", async () => {
+			jest.spyOn(prismaService.user, "findUnique").mockRejectedValueOnce(new Error());
+
+			const promise = sut.loadByEmail(email);
+
+			await expect(promise).rejects.toThrow();
 		});
 	});
 });
