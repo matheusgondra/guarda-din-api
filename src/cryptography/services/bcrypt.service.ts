50c4f8cc6b5c4dbb9b7644946d9c5dd81e38248e
matheusgondra <matheusgondra951@gmail.com>
2026-04-23 12:49:35 -0300

    feat: ensure BCryptService call bcrypt.compare with correct values

--- src/cryptography/services/bcrypt.service.ts
+++ src/cryptography/services/bcrypt.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import bcrypt from "bcryptjs";
+import { HashComparer } from "../protocols/hash-comparer.protocol";
 import { HashGenerator } from "../protocols/hash-generator.protocol";
 
 @Injectable()
-export class BCryptService implements HashGenerator {
+export class BCryptService implements HashGenerator, HashComparer {
 	constructor(private readonly salt: number) {}
 
 	async generate(value: string): Promise<string> {
 		return bcrypt.hash(value, this.salt);
+	}
+
+	async compare(value: string, hash: string): Promise<boolean> {
+		return bcrypt.compare(value, hash);
 	}
 }
