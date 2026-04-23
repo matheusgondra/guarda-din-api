905e66c9eaad6e85c5919d377b1aa0b8ec1985b3
matheusgondra <matheusgondra951@gmail.com>
2026-04-23 12:33:39 -0300

    feat: ensure LoginService throws InvalidCredentialsError if LoadUserByEmailRepository returns null

--- src/auth/services/login.service.ts
+++ src/auth/services/login.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials.error";
 import { LoginParam, LoginResult, LoginUseCase } from "@/domain/usecases/login.usecase";
 import { LoadUserByEmailRepository } from "@/user/protocols/load-user-by-email-repository.protocol";
 
@@ -7,7 +8,10 @@ export class LoginService implements LoginUseCase {
 	constructor(private readonly loadUserByEmailRepository: LoadUserByEmailRepository) {}
 
 	async execute({ email }: LoginParam): Promise<LoginResult> {
-		await this.loadUserByEmailRepository.loadByEmail(email);
+		const user = await this.loadUserByEmailRepository.loadByEmail(email);
+		if (!user) {
+			throw new InvalidCredentialsError();
+		}
 		return {} as LoginResult;
 	}
 }
