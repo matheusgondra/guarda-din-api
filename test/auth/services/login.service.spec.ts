ad19f26aefa71d1085fbe7fd38c4f8d10db29bb4
matheusgondra <matheusgondra951@gmail.com>
2026-04-23 12:44:56 -0300

    test: ensure LoginService call TokenGenerator with correct value

--- test/auth/services/login.service.spec.ts
+++ test/auth/services/login.service.spec.ts
@@ -2,6 +2,7 @@ import { Test } from "@nestjs/testing";
 import { UserMock } from "@test/user/mock/user.mock";
 import { LoginService } from "@/auth/services/login.service";
 import { HashComparer } from "@/cryptography/protocols/hash-comparer.protocol";
+import { TokenGenerator } from "@/cryptography/protocols/token-generator.protocol";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials.error";
 import { LoginParam } from "@/domain/usecases/login.usecase";
 import { LoadUserByEmailRepository } from "@/user/protocols/load-user-by-email-repository.protocol";
@@ -16,6 +17,7 @@ describe("LoginService", () => {
 	let sut: LoginService;
 	let loadUserByEmailRepository: LoadUserByEmailRepository;
 	let hashComparer: HashComparer;
+	let tokenGenerator: TokenGenerator;
 
 	beforeEach(async () => {
 		const module = await Test.createTestingModule({
@@ -32,6 +34,12 @@ describe("LoginService", () => {
 					useValue: {
 						compare: jest.fn().mockResolvedValue(true)
 					}
+				},
+				{
+					provide: TokenGenerator,
+					useValue: {
+						generateToken: jest.fn().mockResolvedValue("anyToken")
+					}
 				}
 			]
 		}).compile();
@@ -39,53 +47,68 @@ describe("LoginService", () => {
 		sut = module.get<LoginService>(LoginService);
 		loadUserByEmailRepository = module.get<LoadUserByEmailRepository>(LoadUserByEmailRepository);
 		hashComparer = module.get<HashComparer>(HashComparer);
+		tokenGenerator = module.get<TokenGenerator>(TokenGenerator);
 	});
 
-	it("Should call LoadUserByEmailRepository with correct email", async () => {
-		const loadByEmailSpy = jest.spyOn(loadUserByEmailRepository, "loadByEmail");
+	describe("LoadUserByEmailRepository", () => {
+		it("Should call LoadUserByEmailRepository with correct email", async () => {
+			const loadByEmailSpy = jest.spyOn(loadUserByEmailRepository, "loadByEmail");
 
-		await sut.execute(param);
+			await sut.execute(param);
 
-		expect(loadByEmailSpy).toHaveBeenCalledWith(param.email);
+			expect(loadByEmailSpy).toHaveBeenCalledWith(param.email);
+		});
+
+		it("Should throw if LoadUserByEmailRepository throws", async () => {
+			jest.spyOn(loadUserByEmailRepository, "loadByEmail").mockRejectedValueOnce(new Error());
+
+			const promise = sut.execute(param);
+
+			await expect(promise).rejects.toThrow();
+		});
+
+		it("Should throw InvalidCredentialsError if LoadUserByEmailRepository returns null", async () => {
+			jest.spyOn(loadUserByEmailRepository, "loadByEmail").mockResolvedValueOnce(null);
+
+			const promise = sut.execute(param);
+
+			await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+		});
 	});
 
-	it("Should throw if LoadUserByEmailRepository throws", async () => {
-		jest.spyOn(loadUserByEmailRepository, "loadByEmail").mockRejectedValueOnce(new Error());
+	describe("HashComparer", () => {
+		it("Should call HashComparer with correct values", async () => {
+			const compareSpy = jest.spyOn(hashComparer, "compare");
 
-		const promise = sut.execute(param);
+			await sut.execute(param);
 
-		await expect(promise).rejects.toThrow();
+			expect(compareSpy).toHaveBeenCalledWith(param.password, userMock.getPassword());
+		});
+
+		it("Should throw if HashComparer throws", async () => {
+			jest.spyOn(hashComparer, "compare").mockRejectedValueOnce(new Error());
+
+			const promise = sut.execute(param);
+
+			await expect(promise).rejects.toThrow();
+		});
+
+		it("Should throw InvalidCredentialsError if HashComparer returns false", async () => {
+			jest.spyOn(hashComparer, "compare").mockResolvedValueOnce(false);
+
+			const promise = sut.execute(param);
+
+			await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+		});
 	});
 
-	it("Should throw InvalidCredentialsError if LoadUserByEmailRepository returns null", async () => {
-		jest.spyOn(loadUserByEmailRepository, "loadByEmail").mockResolvedValueOnce(null);
+	describe("TokenGenerator", () => {
+		it("Should call TokenGenerator with correct value", async () => {
+			const generateTokenSpy = jest.spyOn(tokenGenerator, "generateToken");
 
-		const promise = sut.execute(param);
+			await sut.execute(param);
 
-		await expect(promise).rejects.toThrow(new InvalidCredentialsError());
-	});
-
-	it("Should call HashComparer with correct values", async () => {
-		const compareSpy = jest.spyOn(hashComparer, "compare");
-
-		await sut.execute(param);
-
-		expect(compareSpy).toHaveBeenCalledWith(param.password, userMock.getPassword());
-	});
-
-	it("Should throw if HashComparer throws", async () => {
-		jest.spyOn(hashComparer, "compare").mockRejectedValueOnce(new Error());
-
-		const promise = sut.execute(param);
-
-		await expect(promise).rejects.toThrow();
-	});
-
-	it("Should throw InvalidCredentialsError if HashComparer returns false", async () => {
-		jest.spyOn(hashComparer, "compare").mockResolvedValueOnce(false);
-
-		const promise = sut.execute(param);
-
-		await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+			expect(generateTokenSpy).toHaveBeenCalledWith(userMock.getId());
+		});
 	});
 });
