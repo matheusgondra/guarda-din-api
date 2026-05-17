036ddcbf36d4842b2980d8d9cf93454cadd69736
matheusgondra <matheusgondra951@gmail.com>
2026-05-17 09:02:33 -0300

    test: ensure LoadExpenseService call LoadExpenseRepository with correct values

---
+++ test/expense/service/load-expense.service.spec.ts
@@ -0,0 +1,44 @@
+import { Test } from "@nestjs/testing";
+import { LoadExpenseParam } from "@/domain/usecases/expense/load-expense.usecase";
+import { LoadExpenseRepository } from "@/expense/protocols/load-expense-repository.protocol";
+import { LoadExpenseService } from "@/expense/services/load-expense.service";
+
+describe("LoadExpenseService", () => {
+	let sut: LoadExpenseService;
+	let loadExpenseRepository: LoadExpenseRepository;
+
+	const param: LoadExpenseParam = {
+		userId: "any_user_id",
+		page: 1,
+		pageSize: 10
+	};
+
+	beforeEach(async () => {
+		const module = await Test.createTestingModule({
+			providers: [
+				LoadExpenseService,
+				{
+					provide: LoadExpenseRepository,
+					useValue: {
+						load: jest.fn()
+					}
+				}
+			]
+		}).compile();
+
+		sut = module.get<LoadExpenseService>(LoadExpenseService);
+		loadExpenseRepository = module.get<LoadExpenseRepository>(LoadExpenseRepository);
+	});
+
+	it("Should call LoadExpenseRepository with correct values", async () => {
+		const loadSpy = jest.spyOn(loadExpenseRepository, "load");
+
+		await sut.execute(param);
+
+		expect(loadSpy).toHaveBeenCalledWith({
+			userId: param.userId,
+			page: param.page,
+			limit: param.pageSize
+		});
+	});
+});
